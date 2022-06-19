11e990f2b69f1d7ad097a29a8ffe56ba0805c705
Alysia Wu <alysiawu@Alysias-MacBook-Pro.local>
2022-06-19 02:33:40 -0700

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react'
+import SmoothScroll from 'smooth-scroll'
 import { Navigation } from './components/navigation'
 import { Header } from './components/header'
 import { Features } from './components/features'
@@ -8,7 +9,7 @@ import { Gallery } from './components/gallery'
 import { Testimonials } from './components/testimonials'
 import { Footer } from './components/footer'
 import JsonData from './data/data.json'
-import SmoothScroll from 'smooth-scroll'
+
 
 export const scroll = new SmoothScroll('a[href*="#"]', {
   speed: 1000,
